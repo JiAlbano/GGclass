385947bafa8749e2d00625eeedb8f32807435361
GG Automation <delunakathleen3@gmail.com>
2024-10-10 11:07:49 +0800

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,18 +13,19 @@
 
     {{-- Display errors if any occurred during login --}}
     @if ($errors->any())
-        <div class="alert alert-danger">
+        <div class="alert alert-danger" style="position: absolute; top: 30px; left: 50%; transform: translateX(-50%); width: 80%; text-align: center; color: white; font-family: 'Press Start 2P', cursive;">
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li> <!-- List each error message -->
                 @endforeach
             </ul>
         </div>
+
     @endif
 
     {{-- Display a success message if any --}}
     @if(session()->has('message'))
-    <div class="alert alert-success">
+    <div class="alert alert-success" style="position: absolute; top: 30px; left: 50%; transform: translateX(-50%); width: 80%; text-align: center; color:white; font-family: 'Press Start 2P', cursive;">
         {{ session()->get('message') }} <!-- Show the success message -->
     </div>
     @endif
